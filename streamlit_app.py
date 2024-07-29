b630db1d0a24cd4cc898ad3c7db0be1bd1e96c4f
Dhanush-klm <167899716+Dhanush-klm@users.noreply.github.com>
2024-07-29 17:22:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,7 @@ def truncate_text(text, max_tokens):
     tokens = tokenizer.encode(text)
     return tokenizer.decode(tokens[:max_tokens])
 
+
 # Function to count tokens
 def num_tokens_from_string(string: str, encoding_name: str = "cl100k_base") -> int:
     encoding = tiktoken.get_encoding(encoding_name)
